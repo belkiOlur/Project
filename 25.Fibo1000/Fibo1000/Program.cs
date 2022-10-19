c876bcc3a889504159f7286c2efc369601547ef9
belkiOlur <ibrahim.gun@ogr.iu.edu.tr>
2022-10-19 22:20:56 +0300

    Project Euler 25

---
+++ 25.Fibo1000/Fibo1000/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+BigInteger a = 0;
+BigInteger b = 1;
+BigInteger fibo = 0;
+BigInteger fibo1 = 0;
+int sayac = 0;
+int sayac1 = 0;
+for (int i = 1; i > 0; i++)
+{
+
+    fibo = a + b;
+    a = b;
+    b = fibo;
+    sayac++;
+    sayac1 = 0;
+    fibo1 = fibo;
+    while (fibo1 > 0)
+    {
+        fibo1 = fibo1 / 10;
+        sayac1++;
+    }
+    if (sayac1 == 1000)
+    {
+        Console.WriteLine(sayac+1);
+        
+    }
+}
+
+
+
+Console.ReadKey(true);
+Console.Clear();
+
+
